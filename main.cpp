0a79b22c4152d6f429e094ff0648ec5073542cc2
bsee25009-cloud <bsee25009@itu.edu.pk>
2026-05-09 02:33:30 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "function.h"
+#include<iostream>
+using namespace std;
+int main() {
+    int length = 0;
+    person * users = new person[length];
+    signUp(users, length);
+    return 0;
+}
