24c123bb1cf482125340728fe7c5e3eed6dd05e7
bsee25009-cloud <bsee25009@itu.edu.pk>
2026-05-09 02:41:41 +0500

    removed folder

--- cp project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "function.h"
-#include<iostream>
-using namespace std;
-int main() {
-    int length = 0;
-    person * users = new person[length];
-    signUp(users, length);
-    return 0;
-}
