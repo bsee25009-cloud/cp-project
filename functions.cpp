392bdd7fd573697f83aa912be0f1db9712113f1d
bsee25009-cloud <bsee25009@itu.edu.pk>
2026-05-10 14:20:43 +0500

    change

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,5 @@
 #include "function.h"
-
+using namespace std;
 
 void resize(person*& users, int & length, int increment){
     
@@ -20,6 +20,7 @@ void signUp(person*& users, int & length){
     bool validUser = true;
     do {
         cout<<"Enter Name: "<<endl;
+        cin.ignore();
         getline(cin,newperson.name);
         cout<<"Enter Email: "<<endl;
         getline(cin,newperson.email);
@@ -34,14 +35,14 @@ void signUp(person*& users, int & length){
     } while (validUser == false);
     
     ofstream f("users.txt", ios::app);
-    f << newperson.name << "," << newperson.email << "," <<newperson.password << "," << newperson.country << "\n";
+    f << newperson.name<<","<< newperson.email<<","<<newperson.password<<","<< newperson.country << "\n";
     
     cout << "\nAccount created! (" << newperson.name << ", " << newperson.country << ")\n";
     
 }
 
 bool validateUser(person newuser, person * users, int length) {
-    // TODO: add email unique check
+    
     if (newuser.name.length() == 0) {
         cout<<"Name cannot be empty"<<endl;
         return false;
@@ -69,6 +70,13 @@ bool validateUser(person newuser, person * users, int length) {
         return false;
     }
 
+    bool checkEmail = emailExists(newuser.email,users, length);
+
+    if (checkEmail) {
+        cout<<"Email already exists"<<endl;
+        return false;
+    }
+
     if (newuser.password.length() < 8) {
         cout<<"Password must be atleast 8 characters"<<endl;
         return false;
@@ -81,4 +89,65 @@ bool validateUser(person newuser, person * users, int length) {
    
 
     return true;
+}
+bool emailExists(string email,person* users, int length){
+        for (int i = 0; i < length; i++) {
+        if (users[i].email == email)
+            return true;
+    }
+    return false;
+}
+
+void login(person* users, int length) {
+    string email, password;
+
+    cout << "Enter email: ";
+    cin >> email;
+    cout << "Enter password: ";
+    cin >> password;
+
+    for (int i = 0; i < length; i++) {
+        if (users[i].email == email && users[i].password == password) {
+            cout << "Login successful Welcome, " << users[i].name << "\n";
+            return;
+        }
+    }
+    cout << "Invalid email or password.\n";
+}
+
+void loadUsers(person*& users, int& length) {
+    ifstream file("users.txt");
+    if (!file) {
+        cout << "Error: could not open users.txt\n";
+        length = 0;
+        users = nullptr;
+        return;
+    }
+
+    string line;
+    length = 0;
+    while (getline(file, line))
+        if (!line.empty()) length++;
+
+    users = new person[length];
+    file.clear();
+    file.seekg(0);
+
+    int i = 0;
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        // Remove Windows carriage return if present
+        if (!line.empty() && line.back() == '\r')
+            line.pop_back();
+
+        stringstream ss(line);
+        getline(ss, users[i].name,     ',');
+        getline(ss, users[i].email,    ',');
+        getline(ss, users[i].password, ',');
+        getline(ss, users[i].country,  ',');
+        i++;
+    }
+
+    file.close();
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 int main() {
     int length = 0;
     person * users = new person[length];
+    loadUsers(users, length);
+    //cout<<length<<users[0].email<<users[0].password;
+    cout<<   "Main Menu"   <<endl;
+    cout<<"Press 1 to login "<<endl<<"Press 2 to sign up"<<endl<<"Press 0 to exit"<<endl;
+    int choice;
+    cin>>choice;
+    switch(choice)
+{
+    case 1:{
+    login(users,length);
+    break;
+    }
+    case 2:{
     signUp(users, length);
+    break;
+    }
+    case 0:{
+    break;
+    }
+}
     return 0;
 }
+
+
+
