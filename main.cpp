023706c5c29de7db12f2c9ce5342e27dc87c7761
bsee25055 <bsee25055@itu.edu.pk>
2026-05-10 00:22:47 -0700

    commit-2 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
-#include "function.h"
+#include "functions.h"
 #include<iostream>
 using namespace std;
 int main() {
     int length = 0;
     person * users = new person[length];
     signUp(users, length);
-    return 0;
+
+    int choice;
+do{
+    cout <<"\n CONNECTPLUS ADMIN \n";
+    cout << "1. Report \n";
+    cout << "2. Application \n";
+    cout << "0. Exit\n";
+    cin >> choice;
+
+    if(choice==1) 
+    reportMenu();
+    else if(choice==2)
+    marketingMenu();
+        
+} while (choice!=0);
+
+delete[] users;
+
+return 0;
 }
